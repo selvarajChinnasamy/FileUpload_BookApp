edd5dd8b6ae622af0afee874734fa864aaad59e1
selvarajChinnasamy <selvaraj1234597@gmail.com>
2017-10-23 21:28:27 +0530

    ConfigCommit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use('/api',api);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -25,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/api',api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
